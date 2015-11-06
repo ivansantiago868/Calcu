68238f68cfc5e2e954eec655b98c1dbb473fb79e
KIVANDY-PC <KIVANDY-PC@KIVANDY-PC-HP>
2015-11-05 20:51:03 -0500

    prueba 1

--- calculadora/src/java/org/primefaces/showcase/view/button/ButtonView.java
+++ calculadora/src/java/org/primefaces/showcase/view/button/ButtonView.java
@@ -9,7 +9,9 @@ import javax.faces.event.ActionEvent;
  
 @ManagedBean
 public class ButtonView {
-
+    private int ban;
+    private int posi;
+    public String resp;
     private String text;
  
     public String getText() {
@@ -17,31 +19,92 @@ public class ButtonView {
     }
     public void setText(String text) {
             this.text = text;
-    }
-    Operacion oper = new Operacion();
+    }    
+    
         
     public void buttonAction(ActionEvent actionEvent) {
+        ban = 0;
+        
         String nombreCampo =actionEvent.getComponent().getId();
         String id = nombreCampo.substring(1,2);
         String tabla = getText();
-        oper = Modificaion(oper,id,tabla);
+        Modificaion(id,tabla);
         validTablero(id,tabla);
-        
     }
-    public Operacion Modificaion(Operacion opr,String id,String tabla)
+    public void Modificaion(String id,String tabla)
     {
+        String cade;
         if(id.equals("D") || id.equals("M") || id.equals("S") || id.equals("R") || id.equals("I") || id.equals("P"))
         {
-            if(tabla.length() != 0)
+            if(id.equals("I"))
             {
-                if(opr.numero1.length()==0)
+                if(tabla.indexOf("/") != -1 || tabla.indexOf("*") != -1 || tabla.indexOf("+") != -1 || tabla.indexOf("-") != -1)
                 {
-                    opr.numero1 = tabla;
-                    opr.operador = id;
+                    posi = buscarCaracter(tabla);
+                    if(tabla.length() > posi+1)
+                    {
+                        resp=cuadrarDatos(tabla,posi);
+                    }
+                    ban = 2;
+                }
+            }
+            else
+            {
+                if(tabla.indexOf("/") != -1 || tabla.indexOf("*") != -1 || tabla.indexOf("+") != -1 || tabla.indexOf("-") != -1)
+                {
+                    ban = 1;
                 }
             }
         }
-        return opr;
+    }
+    public String cuadrarDatos(String tabla,int posi)
+    {
+        int num1,num2;
+        int total = tabla.length();
+        num1 = Integer.parseInt(tabla.substring(0,posi));
+        num2 = Integer.parseInt(tabla.substring(posi+1,total));
+        double Total =  opera(num1,num2,tabla.substring(posi,posi+1));
+        String tot = new Double(Total).toString();
+        return tot;
+    }
+    public double opera(int num1,int num2,String operador)
+    {
+        double total = 0;
+        switch(operador){
+            case "/":
+                total = num1 / num2;
+            break;
+            case "*":
+                total = num1 * num2;
+            break;
+            case "+":
+                total = num1 + num2;
+            break;
+            case "-":
+                total = num1 - num2;
+            break;
+        }
+        return total;
+    }
+    public int buscarCaracter(String tabla){
+        int con = 0;
+        if(tabla.indexOf("/") != -1)
+        {
+            return tabla.indexOf("/");    
+        }  
+        if(tabla.indexOf("*") != -1)
+        {
+            return tabla.indexOf("*");    
+        }   
+        if(tabla.indexOf("-") != -1)
+        {
+            return tabla.indexOf("-");    
+        }   
+        if(tabla.indexOf("+") != -1)
+        {
+            return tabla.indexOf("+");    
+        }   
+        return con;
     }
     public void validTablero(String id,String tabla)
     {
@@ -52,9 +115,41 @@ public class ButtonView {
                 id = "";
             }
         }
-        if(tabla == null){tabla = id;}
-        else{tabla = tabla+id;}
+        if(tabla == null)
+            {tabla = id;}
+        else
+        {
+            if(ban == 0)
+            {
+            tabla = tabla+letra(id);
+            }
+            if(ban == 2)
+            {
+              tabla=  resp;
+            }
+        }
         setText(tabla);
+    }
+    public String letra(String id)
+    {
+        String retur = null;
+        switch(id){
+            case "D":
+                retur= "/";
+            break;
+            case "M":
+                retur= "*";
+            break;
+            case "S":
+                retur= "+";
+            break;
+            case "R":
+                retur= "-";
+            break;
+            default: retur = id;
+            break;
+        }
+        return retur;
     }
      
 }
--- calculadora/src/java/org/primefaces/showcase/view/button/Operacion.java
+++ calculadora/src/java/org/primefaces/showcase/view/button/Operacion.java
@@ -11,32 +11,6 @@ package org.primefaces.showcase.view.button;
  */
 public class Operacion {
     
-     public String numero1;
-     public String numero2;
-     public String operador;
      
-    public String getNumero1() {
-            return numero1;
-    }
-    public void setNumero1(String numero1) {
-            this.numero1 = numero1;
-    }
-    public String getNumero2() {
-            return numero2;
-    }
-    public void setNumero2(String numero2) {
-            this.numero2 = numero2;
-    }
-    public String getOperador() {
-            return operador;
-    }
-    public void setOperador(String operador) {
-            this.operador = operador;
-    }
         
-    public Operacion() {
-        numero1 = "";
-        numero2 = "";
-        operador = "";
-    }
 }
